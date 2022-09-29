644a2a5253df1287253293578e1cbcb5ae47f9bf
raj3028 <rajeshkumarsahoo006@gmail.com>
2022-09-29 11:58:49 +0530

    29.09.2022

---
+++ src/routes/route.js
@@ -0,0 +1,17 @@
+//=====================Importing Module and Packages=====================//
+const express = require('express');
+const router = express.Router();
+
+
+
+
+
+
+//===================== User Registration(Post API) =====================//
+router.post("/register", createUser)
+
+
+
+
+//=====================Module Export=====================//
+module.exports = router;  
