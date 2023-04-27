2d22b8a4c29ad004baede74e30e7a86e62d11cc7
RAJESH KUMAR SAHOO <107460939+Raj3028@users.noreply.github.com>
2023-04-28 00:42:24 +0530

    Updated Redis Config

--- src/Redis/redis.js
+++ src/Redis/redis.js
@@ -6,12 +6,12 @@ const { promisify } = require('util')
 
 //===================== Connect to Redis =====================//
 const redisClient = redis.createClient(
-    10282,
-    "redis-10282.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+    14522,
+    "redis-14522.c305.ap-south-1-1.ec2.cloud.redislabs.com",
     //"redis-11500.c264.ap-south-1-1.ec2.cloud.redislabs.com",UQZxoZN35xqOEab95z7P1g2JCwEH1wTo
     { no_ready_check: true }
 );
-redisClient.auth("eKZgPvpPIOl7k2z6Boosp2XqggdUlMX1", function (err) {
+redisClient.auth("ewF0W968EG43KXyjKYPrk2OhIi0SajLc", function (err) {
     if (err) throw err;
 });
 
