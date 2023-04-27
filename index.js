bcd548b4a956f476d04677e69694051246917175
RAJESH KUMAR SAHOO <107460939+Raj3028@users.noreply.github.com>
2023-04-28 00:57:17 +0530

    Updated

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ app.use(
 app.use('/', route)
 
 //===================== It will Handle error When You input Wrong Route =====================//
-app.use(function (req, res) {
-    var err = new Error("Not Found.")
-    err.status = 400
-    return res.status(400).send({ status: "400", msg: "Path not Found." })
-})
+// app.use(function (req, res) {
+//     var err = new Error("Not Found.")
+//     err.status = 400
+//     return res.status(400).send({ status: "400", msg: "Path not Found." })
+// })
 
 
 app.listen(process.env.PORT || 3000, function () {
